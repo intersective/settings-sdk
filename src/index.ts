1b2ab9a021d80cb110fc72531ca6def68d812611
Mihai Petrescu <mihai.petrescu@gmail.com>
2021-02-22 19:07:16 +1100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import Axios, { AxiosRequestConfig } from 'axios';
+import JWT from 'jsonwebtoken';
+
+export class Preferences {
+  protected service: string;
+  protected privateKey: string;
+  protected url: string;
+
+  constructor(privateKey: string, service: string, url?: string) {
+    this.service = service;
+    this.privateKey = privateKey;
+    this.url = url ? url : 'https://preferences.practera.com/api';
+  }
+
+  get(user: string) : Promise<any> {
+    return this.makeApiCall(user, 'GET');
+  }
+
+  save(user: string, preferences: any) : Promise<any> {
+    return this.makeApiCall(user, 'POST', preferences);
+  }
+
+  private createHeaders(user: string, method: 'GET' | 'POST') : AxiosRequestConfig {
+    return {
+      url: this.url,
+      method,
+      timeout: 20000,
+      headers: {
+        service: this.service,
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        apikey: JWT.sign({ role: 'system', user }, this.privateKey, { algorithm: 'RS256' }),
+      },
+    };
+  }
+  private makeApiCall(user: string, method: 'GET' | 'POST', data?: {}) : Promise<any> {
+    const headers = this.createHeaders(user, method);
+
+    return new Promise(resolve => {
+      // Set up the request
+      Axios.request(headers).then(response => {
+        resolve(response.data);
+      });
+    });
+  }
+}
